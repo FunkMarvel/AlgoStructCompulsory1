66c4c0f1699d13465a1f3753da71593f9dd399e2
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-08-22 21:05:15 +0200

    Update main.cpp

--- DynamicArray/DynamicArray/main.cpp
+++ DynamicArray/DynamicArray/main.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 
 
-int BinarySort(DynamicArray<int> Arr);
-
+void BinarySort(DynamicArray<int>& Arr);
 
 
 int main()
@@ -15,39 +14,36 @@ int main()
 
     std::cout << "Size: " << Array.GetSize() << std::endl;
     std::cout << "Capacity: " << Array.GetCapacity() << std::endl;
-    
+
     Array.Append(11);
     std::cout << "Size: " << Array.GetSize() << std::endl;
     std::cout << "Capacity: " << Array.GetCapacity() << std::endl;
-    
+
     Array.Append(12);
     std::cout << "Size: " << Array.GetSize() << std::endl;
     std::cout << "Capacity: " << Array.GetCapacity() << std::endl;
-    
+
     std::cout << Array << std::endl;
     std::cout << Array[1] << std::endl;
 
     std::cout << Array[1] << std::endl;
 
-//Test
-
-    //DynamicArray<int>::BinarySort(Array);
-
-   // cout << Array.BinarySearch(5);
-    cout << BinarySort(Array);
+    //Test
+    cout << DynamicArray<int>::BinarySearch(Array, 5);
+    cout << Array.BinarySearch(5);
+    BinarySort(Array);
     return 0;
 }
 
 
-int BinarySort(DynamicArray<int> Arr)
+void BinarySort(DynamicArray<int>& Arr)
 {
-    
     int n = Arr.GetSize();
     int key;
     cin >> key;
-    
-    int index = DynamicArray<int>::BinarySearch(Arr,n,key);
-    
+
+    int index = DynamicArray<int>::BinarySearch(Arr, key);
+
     if (index != -1)
     {
         cout << key << " Is present in the index:" << index << endl;
@@ -56,6 +52,4 @@ int BinarySort(DynamicArray<int> Arr)
     {
         cout << " Is MISSING from the index!" << endl;
     }
-
-    return 0;
 }
