b207ec612ad037ca44efc6dc8787135c33862f91
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-08-22 12:39:59 +0200

    began making template class

---
+++ DynamicArray/DynamicArray/DynamicArray.h
@@ -0,0 +1,31 @@
+﻿
+template<typename T>
+class DynamicArray
+{
+public:
+    DynamicArray();
+
+    ~DynamicArray();
+
+private:
+    // internal data array:
+    T* Data_{nullptr};
+    
+    int GrowthFactor_{2};  // amount to increase capacity by when reallocating.
+    int Capacity{1};  // initial amount elements to allocate data for.
+    int Size{0};  // initial amount of elements in DynamicArray.
+};
+
+template <typename T>
+DynamicArray<T>::DynamicArray()
+{
+    // allocate private data array:
+    Data_ = new T[Capacity];
+}
+
+template <typename T>
+DynamicArray<T>::~DynamicArray()
+{
+    // deallocate data array on destruction:
+    delete[] Data_;
+}
