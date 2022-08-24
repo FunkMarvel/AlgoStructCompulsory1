045fc25ead437efe2caa5a69f96a6ec11b83b8cc
Thomasdalsae <72367829+Thomasdalsae@users.noreply.github.com>
2022-08-24 12:18:57 +0200

    SelectionSort,BubbleSort

--- DynamicArray/DynamicArray/main.cpp
+++ DynamicArray/DynamicArray/main.cpp
@@ -9,12 +9,16 @@ void LinearSort(DynamicArray<int>& Arr);
 
 
 void BinarySort(DynamicArray<int>& Arr);
+void selectionSort(DynamicArray<int> &arr);
+
+void BubbleSort(DynamicArray<int> arr);
+bool CompareBubbleSort(int a, int b);
 
 
 
 int main()
 {
-    DynamicArray<int> Array = {3, 4, 5};
+    DynamicArray<int> Array = {3, 2, 1,7,9,19,55};
 
     std::cout << "Size: " << Array.GetSize() << std::endl;
     std::cout << "Capacity: " << Array.GetCapacity() << std::endl;
@@ -29,16 +33,16 @@ int main()
 
     std::cout << Array << std::endl;
     std::cout << Array[1] << std::endl;
-    std::cout << Array.RemoveLastElement() << std::endl;
-    std::cout << Array.RemoveLastElement() << std::endl;
-    std::cout << Array.RemoveLastElement() << std::endl;
+     std::cout << Array.RemoveLastElement() << std::endl;
+     //std::cout << Array.RemoveLastElement() << std::endl;
+     //std::cout << Array.RemoveLastElement() << std::endl;
     std::cout << Array<< std::endl;
     std::cout << "Size: " << Array.GetSize() << std::endl;
     std::cout << "Capacity: " << Array.GetCapacity() << std::endl;
 
     //Test
-    cout << DynamicArray<int>::BinarySearch(Array, 5);
-    cout << Array.BinarySearch(5);
+    //cout << DynamicArray<int>::BinarySearch(Array, 4);
+    //cout << Array.BinarySearch(5);
     // BinarySort(Array);
     //cout << DynamicArray<int>::LinearSearch(Array,5); //Static Version
     //cout << Array.LinearSearch(5); //Instance Version
@@ -47,6 +51,9 @@ int main()
     //cout << DynamicArray<int>::BinarySearch(Array, 5); //Static Version
     //cout << Array.BinarySearch(5); //Instance Version
     //BinarySort(Array); //Function Version
+
+    //selectionSort(Array);
+    BubbleSort(Array);
     
     return 0;
 }
@@ -87,3 +94,72 @@ void LinearSort(DynamicArray<int>& Arr)
         cout << " Is MISSING from the index!" << endl;
     }
 }
+void selectionSort(DynamicArray<int> &arr)
+{
+    int n = arr.GetSize();
+    for (int i = 0;i<= n-2;i++)
+    {
+        int current = arr[i];
+        int currentMin = i;
+
+        //Find the minimal element
+        for (int j=i;j < n;j++)
+        {
+            if (arr[j] < arr[currentMin])
+            {
+                currentMin = j;
+            }
+        }
+        //swap outside the loop
+        swap(arr[currentMin],arr[i]);  
+        
+    }
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+
+    
+}
+
+bool CompareBubbleSort(int a, int b)
+{
+    //Here i can decided  > if i want it inn a deacreasing order or < increasing order;
+  
+    return a > b;
+}
+void BubbleSort(DynamicArray<int> arr)
+{
+    
+    int n = arr.GetSize();
+    int TimeSwapped{};
+    //sort the elements in an increasing order
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int swapped = 0;
+        //Repeated swapping, {5-3-2} - 5->3 - {3,5,2} - 5->2 - {3-2-5} - 3->2 - {2,3,5}
+        //With adding -i-1 we decrease the amount times it need to go over.
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            
+            if (CompareBubbleSort(arr[j],arr[j + 1]))
+            {
+                swap(arr[j + 1], arr[j]);
+                swapped = 1;
+                
+             TimeSwapped++;
+            }
+            
+        }
+        if (swapped == 0)
+            break;
+        
+    }
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+    cout << endl;
+    cout <<"How many Swaps arr needed :" << TimeSwapped << endl;
+}
+
